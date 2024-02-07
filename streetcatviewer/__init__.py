ce32829be22589182b863b028e9540fbc672e244
aguywhosaguy <caseypeg8@gmail.com>
2024-02-06 21:11:34 -0600

    feat: added global variables and database functions

---
+++ streetcatviewer/__init__.py
@@ -0,0 +1,33 @@
+FEEDER, ABOVE, BEHIND = ('0.m3u8', '1.m3u8', '2.m3u8')
+
+JSON_FILE = "streetCams.json"
+
+CAT_STREAMS = [
+    {
+        "baseURL": "http://streetcatpull.hellobike.com/live/4258783365322591678_"
+    },
+    {
+        "nickname": "Mr. Fresh's Other Feeder",
+        "baseURL": "http://streetcatpull.hellobike.com/live/4412424173050749216_"
+    },
+    {
+        "nickname": "Mr. Sonic (Hedgehogs) Feeder",
+        "baseURL": "http://streetcatpull.hellobike.com/live/4303090694701059290_"
+    },
+    {
+        "nickname": "Ms. Sleep's Feeder",
+        "baseURL": "http://streetcatpull.hellobike.com/live/4489918405732275808_"
+    },
+    {
+        "nickname": "Duck Feeder",
+        "baseURL": "http://streetcatpull.hellobike.com/live/4300845904274638881_"
+    },
+    {
+        "nickname": "Mr. Snack's Feeder",
+        "baseURL": "http://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8"
+    },
+    {
+        "nickname": "Mr. Shock's Feeder",
+        "baseURL": "http://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8"
+    }
+]
---
+++ streetcatviewer/database.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+getCamsURL = "https://api.aguywhosaguy.com/api/collections/streetCams/records"
+dataJSON = "streetCams.json"
+
+def get_cams():
+    try:
+        items = requests.get(getCamsURL).json()['items']
+        return [{'baseURL': item['baseURL'], 'nickname': item["nickname"]} for item in items]
+    except:
+        raise Exception("Database failed to return")
+
+def cam_gen():
+    with open(dataJSON, 'w') as jsonFile:
+        jsonFile.write(json.dumps(get_cams(), indent=1))
